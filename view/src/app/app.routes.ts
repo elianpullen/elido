385eef242aa35cb194a55c7b2eb6e4afa45c7e12
elian <elianpullen@protonmail.com>
2025-08-13 13:37:21 +0200

    feat: add (material) test component

--- view/src/app/app.routes.ts
+++ view/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Task } from './components/task/task';
 import { Home } from './components/home/home';
+import { Test } from './components/test/test';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'task', component: Task },
+    { path: 'test', component: Test },
 ];
